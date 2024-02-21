0ea2407dc0ecbfd3ce9ccbad3ca9466f3bc22887
sBenicioDev <sjbenicio@hotmail.com>
2024-02-20 21:15:09 -0300

    Primer Commit

---
+++ src/main/java/dev/sbenicio/webappmaxcotas/WebappMaxcotasApplication.java
@@ -0,0 +1,13 @@
+package dev.sbenicio.webappmaxcotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappMaxcotasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappMaxcotasApplication.class, args);
+    }
+
+}
