6832261b17e1f1213d3a678e9ef677b55de928ed
sBenicioDev <sjbenicio@hotmail.com>
2024-02-29 21:04:02 -0300

    Agregado Auth Controller

--- src/main/java/dev/sbenicio/webappmaxcotas/controller/AuthController.java
+++ src/main/java/dev/sbenicio/webappmaxcotas/controller/AuthController.java
@@ -1,9 +1,13 @@
 package dev.sbenicio.webappmaxcotas.controller;
 
+import dev.sbenicio.webappmaxcotas.model.Usuario;
 import dev.sbenicio.webappmaxcotas.service.CustomUserDetailsService;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
@@ -16,5 +20,29 @@ public class AuthController {
         return "redirect:/mascotas";
     }
 
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/registro")
+    public String mostrarFormularioRegistro(Model model){
+        model.addAttribute("usuario", new Usuario());
+
+        return "registro";
+    }
+
+    @PostMapping("/registro")
+    public String registrarUsuario(Usuario usuario){
+        customUserDetailsService.guardarUsuario(usuario);
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/gestorRoles")
+    public String gestorRoles(Model model) {
+        model.addAttribute("usuarios", customUserDetailsService.listarUsuariosRegistrados());
+        return "gestorRoles";
+    }
 
 }
